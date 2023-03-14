acc0903963c4d681c9db7d4dadd89616123e174f
Nenad Sandic <Nenad.Sandic@vanderlande.com>
2023-03-14 18:19:21 +0100

    Section 07: 71-Refactoring the app to use MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
-import { Button, Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { v4 as uuid } from 'uuid'
 import agent from '../api/agent';
@@ -10,26 +10,16 @@ import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
 
 function App() {
-  const {activityStore} = useStore();
+  const { activityStore } = useStore();
 
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [editMode, setEditMode] = useState(false);  
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list()
-      .then(response => {
-        let activities: Activity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('T')[0];
-          activities.push(activity);
-        })
-        setActivities(activities);
-        setLoading(false);
-      })
-  }, [])
+    activityStore.loadActivities();
+  }, [activityStore])
 
   function handleSelectActivity(id: string) {
     setSelectedActivity(activities.find(x => x.id === id))
@@ -50,11 +40,11 @@ function App() {
 
   function handleDeleteActivity(id: string) {
     setSubmitting(true);
-    agent.Activities.delete(id).then(()=>{
+    agent.Activities.delete(id).then(() => {
       setActivities([...activities.filter(x => x.id !== id)]);
       setSubmitting(false);
     })
-    
+
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
@@ -77,16 +67,14 @@ function App() {
     }
   }
 
-  if (loading) return <LoadingComponent content='Loading app' />
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
-        <h2>{activityStore.title}</h2>
-        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} />
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
